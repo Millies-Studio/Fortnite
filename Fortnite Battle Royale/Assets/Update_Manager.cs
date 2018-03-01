e28076fecb51f380ec118a535ed438a9bc6e713d
MilliesStudio <36960975+MilliesStudio@users.noreply.github.com>
2018-03-01 15:30:18 +0000

    o.1

---
+++ Fortnite Battle Royale/Assets/Update_Manager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class Update_Manager : MonoBehaviour
+{
+    public Text text;
+
+    public string url = "https://github.com/Millies-Studio/Fortnite.git";
+    IEnumerator Start()
+    {
+        using (WWW www = new WWW(url))
+        {
+            yield return www;
+
+        }
+    }
+}
